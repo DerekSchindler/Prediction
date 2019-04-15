79b9dfcf336ef3a4ca14f68c3b259835de29cbb8
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-04-15 08:06:52 -0400

    Edited code to create methods for individual steps

--- src/Prediction.java
+++ src/Prediction.java
@@ -9,10 +9,7 @@ public class Prediction {
         int m;
         int q;
         int year;
-        int J;
-        int K;
         int h;
-        int a, b, c, d, e, f;
         String next;
 
         //rhyme
@@ -44,53 +41,64 @@ public class Prediction {
                 year=year-1;
             }
             //written month
-            switch(m){
-                case 13:System.out.println("There are 31 days in January."); break;
-                case 14:System.out.println("There are 28 days in February."); break;
-                case 3:System.out.println("There are 31 days in March."); break;
-                case 4:System.out.println("There are 30 days in April."); break;
-                case 5:System.out.println("There are 31 days in May."); break;
-                case 6:System.out.println("There are 30 days in June."); break;
-                case 7:System.out.println("There are 31 days in July."); break;
-                case 8:System.out.println("There are 31 days in August."); break;
-                case 9:System.out.println("There are 30 days in September."); break;
-                case 10:System.out.println("There are 31 days in October."); break;
-                case 11:System.out.println("There are 30 days in November."); break;
-                case 12:System.out.println("There are 31 days in December."); break;
-            }
+            month(m);
 
             //Equation
-            K= year%100;
-            J=year/100;
-
-            a = m+1;
-            b = 13*a;
-            c = b/5;
-            d = K/4;
-            e = J/4;
-            f = J*5;
-
-            h = q + c + K + d + e +f;
-            h = h%7;
+            h = equation(m,q,year);
 
             //poem line
-            switch(h){
-                case 0: System.out.println("You were born on a Saturday.\n According to the poem you work hard for a living."); break;
-                case 1:System.out.println("You were born on a Sunday.\n According to the poem you are fair and wise and good in every way."); break;
-                case 2:System.out.println("You were born on a Monday. \n According to the poem you are fair of face."); break;
-                case 3:System.out.println("You were born on a Tuesday. \n According to the poem you are full of grace."); break;
-                case 4:System.out.println("You were born on a Wednesday. \n According to the poem you are full of woe."); break;
-                case 5:System.out.println("You were born on a Thursday. \n According to the poem you have far to go. "); break;
-                case 6:System.out.println("You were born on a Friday. \n According to the poem you are loving and giving. "); break;
-            }
+            poem(h);
 
             System.out.println("Try another date.");
 
         }
 
     }
+    public static void month(int writtenMonth){
+        switch(writtenMonth){
+            case 13:System.out.println("There are 31 days in January."); break;
+            case 14:System.out.println("There are 28 days in February."); break;
+            case 3:System.out.println("There are 31 days in March."); break;
+            case 4:System.out.println("There are 30 days in April."); break;
+            case 5:System.out.println("There are 31 days in May."); break;
+            case 6:System.out.println("There are 30 days in June."); break;
+            case 7:System.out.println("There are 31 days in July."); break;
+            case 8:System.out.println("There are 31 days in August."); break;
+            case 9:System.out.println("There are 30 days in September."); break;
+            case 10:System.out.println("There are 31 days in October."); break;
+            case 11:System.out.println("There are 30 days in November."); break;
+            case 12:System.out.println("There are 31 days in December."); break;
+        }
 
 
+    }
+    public static int equation(int m, int q, int year){
+        int K,J,a,b,c,d,e,f,h;
+        K= year%100;
+        J=year/100;
 
+        a = m+1;
+        b = 13*a;
+        c = b/5;
+        d = K/4;
+        e = J/4;
+        f = J*5;
+
+        h = q + c + K + d + e +f;
+        h = h%7;
+        return h;
+
+    }
+    public static void poem(int h){
+        switch(h){
+            case 0: System.out.println("You were born on a Saturday.\n According to the poem you work hard for a living."); break;
+            case 1:System.out.println("You were born on a Sunday.\n According to the poem you are fair and wise and good in every way."); break;
+            case 2:System.out.println("You were born on a Monday. \n According to the poem you are fair of face."); break;
+            case 3:System.out.println("You were born on a Tuesday. \n According to the poem you are full of grace."); break;
+            case 4:System.out.println("You were born on a Wednesday. \n According to the poem you are full of woe."); break;
+            case 5:System.out.println("You were born on a Thursday. \n According to the poem you have far to go. "); break;
+            case 6:System.out.println("You were born on a Friday. \n According to the poem you are loving and giving. "); break;
+        }
+    }
     }
 
